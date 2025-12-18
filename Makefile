cf311c5b6b3ac79eb66e3772d83f3a743c637d8c
Matt Krueger <mkrueger@rstms.net>
2025-12-17 17:35:22 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,75 @@
+
+# gmail authorization makefile
+
+project != basename $$(pwd)
+gitclean = if git status --porcelain | grep '^.*$$'; then echo git status is dirty; false; else echo git status is clean; true; fi
+
+src = $(wildcard *.js) 
+json != find -type f -name \*.json
+json_fmt = $(foreach foo,$(json),$(dir $(foo)).$(notdir $(basename $(foo))))
+
+html = index.html
+
+package_files = VERSION LICENSE README.md $(src) $(html) assets
+
+version != cat VERSION
+
+all: $(html) $(src) $(json_fmt) fix .fmt lint assets
+	touch manifest.json
+
+.manifest: manifest.json
+	jq . <$< >$<.parsed && mv $<.parsed $<
+	touch $@
+
+.updates: updates.json
+	jq . <$< >$<.parsed && mv $<.parsed $<
+	touch $@
+
+assets: exported/assets
+	rm -rf assets
+	mkdir assets
+	mv exported/assets/* assets
+
+%.html: exported/%.html
+	sed '/<script>/,/<\/script>/d' $< >$@
+
+fix: .fix
+
+.fix: $(src)
+	fix eslint fix *.js
+
+lint: eslint.config.js
+	eslint *.js
+
+eslint.config.js:
+	eslint config >$@
+
+fmt:	.fmt
+
+.fmt: fix $(html)
+	prettier --tab-width 4 --print-width 135 --write "*.js" "*.html"
+	touch $@
+
+release_file = $(project)-$(version).tgz
+
+release: all
+	@$(gitclean) || { [ -n "$(dirty)" ] && echo "allowing dirty release"; }
+	rm -f release.zip
+	zip release.zip -r $(package_files)
+	mv release.zip dist/$(release_file)
+	@$(if $(update),gh release delete -y v$(version),)
+	gh release create v$(version) --notes "v$(version)"
+	gh release upload v$(version) updates.json
+	( cd dist && gh release upload v$(version) $(release_file) )
+
+clean:
+	rm -f .eslint
+	docker rmi eslint || true
+	rm -f .prettier
+	docker rmi prettier || true
+	rm -rf src/node_modules
+	rm -f release.zip
+
+distclean: clean
+	rm -f dist/*.xpi
+
---
+++ index.js
@@ -0,0 +1,134 @@
+/* globals console, document, fetch, URL, URLSearchParams, window */
+
+function hideElement(elementId) {
+    try {
+        document.getElementById(elementId).hidden = true;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function showElement(elementId) {
+    try {
+        document.getElementById(elementId).hidden = false;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function resetPage() {
+    showElement("auth_controls");
+    hideElement("auth_result");
+    hideElement("revoke_instructions");
+    window.location.href = "https://webmail.mailcapsule.io/oauth/";
+}
+
+function showResult(result) {
+    document.getElementById("auth_result_text").textContent = JSON.stringify(result, null, 2);
+    hideElement("auth_controls");
+    showElement("auth_result");
+    if (result.Success) {
+        showElement("revoke_instructions");
+    } else {
+        hideElement("revoke_instructions");
+    }
+}
+
+async function onWindowLoad() {
+    try {
+        console.log("window loaded");
+        await updateUsernames();
+        console.log("href:", window.location.href);
+        const url = new URL(window.location.href);
+        const params = new URLSearchParams(url.search);
+        if (params.toString().length > 0) {
+            const jsonParams = { url: window.location.href };
+            params.forEach((value, key) => {
+                jsonParams[key] = value;
+            });
+            showResult(jsonParams);
+            if (jsonParams["authorization"] === "pending") {
+                const endpoint = "https://webmail.mailcapsule.io/oauth/authorize/";
+                const result = await requestAuthorization(endpoint, { state: jsonParams["state"] });
+                showResult(result);
+            }
+        }
+    } catch (e) {
+        console.error("onWindowLoad:", e);
+    }
+}
+
+async function updateUsernames() {
+    try {
+        console.log("updating usernames");
+        const url = "https://webmail.mailcapsule.io/oauth/usernames/";
+        const response = await fetch(url);
+        const selectTitle = document.getElementById("username_title");
+        const selectElement = document.getElementById("username_select");
+        const usernames = await response.json();
+        //const usernames = [];
+        console.log("usernames:", usernames);
+        if (usernames.length < 1) {
+            hideElement("username_select");
+            selectTitle.textContent = "No eligible usernames exist";
+            console.log("selectTitle:", selectTitle);
+            console.log("selectElement:", selectElement);
+        } else {
+            selectTitle.textContent = "Select a local account:";
+            showElement("username_select");
+        }
+
+        usernames.forEach((username) => {
+            const option = document.createElement("option");
+            option.value = username;
+            option.textContent = username;
+            selectElement.appendChild(option);
+        });
+    } catch (e) {
+        console.error("updateUsernames:", e);
+    }
+}
+
+async function requestAuthorization(url, params) {
+    try {
+        console.log("sending post request:", url, params);
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(params),
+        });
+        return response.json();
+    } catch (e) {
+        console.error("postAuthorizationRequest:", e);
+    }
+}
+
+async function requestAuthentication() {
+    try {
+        const response = await fetch("https://webmail.mailcapsule.io/oauth/authenticate/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: document.getElementById("local_username").value }),
+        });
+        console.log("response:", response);
+        const result = await response.json();
+        console.log("result:", result);
+        const authUri = result.Message;
+        if (result.Success) {
+            console.log("redirecting to:", authUri);
+            window.location.href = authUri;
+        } else {
+            console.log("not redirecting to:", authUri);
+            showResult(result);
+        }
+    } catch (e) {
+        console.error("postAuthenticationRequest:", e);
+    }
+}
+window.onload = onWindowLoad;
+document.getElementById("reset_button").addEventListener("click", resetPage);
+document.getElementById("authenticate_button").addEventListener("click", requestAuthentication);
