ba6a09aa13f3a99d339c22f318811df5f5fea688
Matt Krueger <mkrueger@rstms.net>
2025-12-18 23:25:48 -0700

    improve logging of postAuthenticationRequest data

--- index.js
+++ index.js
@@ -185,14 +185,14 @@ function updateAuthButtons(authEnable, deauthEnable) {
 async function postAuthenticationRequest(uri, enableRedirect) {
     try {
         const selectElement = document.getElementById("username_select");
-        const bodyData = selectElement.value;
+        const bodyData = JSON.stringify(selectElement.value);
         console.log("postAuthenticationRequest:", { uri: uri, body: bodyData });
         const response = await fetch(uri, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(bodyData),
+            body: bodyData,
         });
         console.log("response:", response);
         const result = await response.json();
